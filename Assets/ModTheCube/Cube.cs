51db492ae624314c63c6f5dc81f21940a28127d4
Itsjohanes <johannesap@upi.edu>
2022-08-07 21:06:53 +0700

    First Commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed = 10;
+    
+    void Start()
+    {
+        transform.position = new Vector3(0, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(50.0f * Time.deltaTime, 30.0f *Time.deltaTime, 50.0f * Time.deltaTime);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
